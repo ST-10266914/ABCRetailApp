6bfb276a59a4df73cc4ab013ac39fb1bf4ab2479
Keyur Ramdeen Singh <ST10266914@vcconnect.edu.za>
2024-08-29 14:54:47 +0200

    Add project files.

---
+++ ABCRetailApp/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ABCRetailApp.Models;
+using ABCRetailApp.Services;
+
+namespace ABCRetailApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly TableStorageService<CustomerProfile> _tableStorageService;
+
+        public CustomerController(TableStorageService<CustomerProfile> tableStorageService)
+        {
+            _tableStorageService = tableStorageService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var customers = await _tableStorageService.RetrieveAllEntitiesAsync();
+            return View(customers);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CustomerProfile customer)
+        {
+            if (ModelState.IsValid)
+            {
+                customer.PartitionKey = "CustomerProfile";
+                customer.RowKey = Guid.NewGuid().ToString();
+                await _tableStorageService.InsertOrMergeEntityAsync(customer);
+                return RedirectToAction("Index");
+            }
+            return View(customer);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(string partitionKey, string rowKey)
+        {
+            var customer = new CustomerProfile
+            {
+                PartitionKey = partitionKey,
+                RowKey = rowKey
+            };
+            await _tableStorageService.DeleteEntityAsync(customer);
+            return RedirectToAction("Index");
+        }
+    
+}
+}
---
+++ ABCRetailApp/Controllers/FilesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ABCRetailApp.Services;
+using ABCRetailApp.Models;
+using System.Threading.Tasks;
+
+public class FilesController : Controller
+{
+    private readonly FileStorageService _fileStorageService;
+
+    public FilesController(FileStorageService fileStorageService)
+    {
+        _fileStorageService = fileStorageService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Upload(IFormFile file)
+    {
+        if (file != null && file.Length > 0)
+        {
+            using (var stream = file.OpenReadStream())
+            {
+                await _fileStorageService.UploadFileAsync(file.FileName, stream);
+            }
+        }
+        return RedirectToAction("Index");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Download(string fileName)
+    {
+        var fileStream = await _fileStorageService.DownloadFileAsync(fileName);
+        return File(fileStream, "application/octet-stream", fileName);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var files = await _fileStorageService.ListFilesAsync();
+        return View(files);
+    }
+}
